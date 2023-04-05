0adf8b23a075f93b60b7e081abbf49cd0da7d28e
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-04-05 19:13:10 +0200

    king being dropped

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const squares = document.querySelectorAll('.square')
 
 king.addEventListener('dragstart', dragStart)
 
+squares.forEach(square => {
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+ })
+
+let beingDragged
+
 function dragStart(e) {
-    console.log(e.target)
+    beingDragged = e.target
 }
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+     console.log(e.target)
+}
+
